cce22d6e74019f581c8d252d8c42b9bc791b0d89
Jafar <jafarcees@gmail.com>
2017-03-18 23:53:57 +0530

    bootstrap updated

--- oruma/forms.py
+++ oruma/forms.py
@@ -19,25 +19,25 @@ class Form1(forms.Form):
     applicant_family_earners = forms.IntegerField(label='Number of earners')
 
 class recommenderForm(forms.Form):
-    recommender_first_name = forms.CharField(max_length = 100, label='First Name')
-    recommender_last_name = forms.CharField(max_length=100, label='Second Name')
-    recommender_phone = forms.CharField(max_length=13, label='Phone Number')
-    recommender_email = forms.EmailField(max_length=80, label='Email')
-    recommender_address = forms.CharField(widget = forms.Textarea, max_length=300, label='Address')
+    recommender_first_name = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}),max_length = 100, label='First Name')
+    recommender_last_name = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'Second Name', 'class': 'form-control'}),max_length=100, label='Second Name')
+    recommender_phone = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'Phone Number', 'class': 'form-control'}), max_length=13, label='Phone Number')
+    recommender_email = forms.EmailField(widget = forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}),max_length=80, label='Email')
+    recommender_address = forms.CharField(widget = forms.Textarea(attrs={'placeholder': 'Address', 'class': 'form-control', 'rows':'3'}), max_length=300, label='Address')
 
 
 class applicantForm(forms.Form):
-    applicant_first_name = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'First Name'}), max_length = 100, label='First Name')
-    applicant_second_name = forms.CharField(max_length=100, label='Second Name')
+    applicant_first_name = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}), max_length = 100, label='First Name')
+    applicant_second_name = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'Second Name', 'class': 'form-control'}), max_length=100, label='Second Name')
     SEX = (('M', 'Male'),('F', 'Female'))
 
-    applicant_sex = forms.ChoiceField( choices=SEX, label='Gender')
-    applicant_phone = forms.CharField(max_length=13, label='Phone Number')
-    applicant_bod = forms.DateField( widget = forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'}), label='Date of Birth')
-    applicant_occupation = forms.CharField(max_length=50, label='Occupation')
-    applicant_address = forms.CharField(widget = forms.Textarea, max_length=300, label='Address')
-    applicant_monthly_income =forms.IntegerField(label='Monthly total Income')
-    applicant_family_earners = forms.IntegerField(label='Number of earners')
+    applicant_sex = forms.ChoiceField( widget=forms.Select(attrs={'class': 'form-control'}), choices=SEX, label='Gender')
+    applicant_phone = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'Phone Number', 'class': 'form-control'}), max_length=13, label='Phone Number')
+    applicant_bod = forms.DateField( widget = forms.DateInput(attrs={'placeholder': 'MM/DD/YYYY', 'class': 'form-control'}), label='Date of Birth')
+    applicant_occupation = forms.CharField(widget = forms.TextInput(attrs={'placeholder': 'Occupation', 'class': 'form-control'}), max_length=50, label='Occupation')
+    applicant_address = forms.CharField(widget = forms.Textarea(attrs={'placeholder': 'Address', 'class': 'form-control', 'rows':'3'}), max_length=300, label='Address')
+    applicant_monthly_income =forms.IntegerField(widget = forms.NumberInput(attrs={'placeholder': 'Monthly Income', 'class': 'form-control'}),label='Monthly total Income')
+    applicant_family_earners = forms.IntegerField(widget = forms.NumberInput(attrs={'placeholder': 'Monthly Income', 'class': 'form-control'}), label='Number of earners')
 
 
 class DependendForm(forms.Form):
